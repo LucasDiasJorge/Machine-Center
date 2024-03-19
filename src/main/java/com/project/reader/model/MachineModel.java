2153d4b9bc9dd3fa68e9de4a7991e03a478e2eac
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-03-19 15:53:52 -0300

    Update MachineModel.java

--- src/main/java/com/project/reader/model/MachineModel.java
+++ src/main/java/com/project/reader/model/MachineModel.java
@@ -36,56 +36,12 @@ public class MachineModel extends AbstractModel {
     @Column(name = "serial", nullable = false)
     private String serial;
 
-    @Column(name = "manager", nullable = false)
-    private String manager;
-
     @Column(name = "keep_alive",
             columnDefinition = "timestamp with time zone")
     @Temporal(TemporalType.TIMESTAMP)
     private Date keepAlive;
 
-    @Column(name = "read_timeout", nullable = false,
-            columnDefinition = "int default 2")
-    private int readTimeout;
-
-    @Column(name = "send_period", nullable = false,
-            columnDefinition = "int default 5")
-    private int sendPeriod;
-
-    @Column(name = "model", nullable = false)
-    private String model;
-
-    @Column(name = "writer")
-    private boolean writer;
-
-    @Column(name = "product_id_writer")
-    private Long productId;
-
     public MachineModel() {
-    }
-
-    public Long getProductId() {
-        return productId;
-    }
-
-    public void setProductId(Long productId) {
-        this.productId = productId;
-    }
-
-    public boolean isWriter() {
-        return writer;
-    }
-
-    public void setWriter(boolean writer) {
-        this.writer = writer;
-    }
-
-    public String getManager() {
-        return manager;
-    }
-
-    public void setManager(String manager) {
-        this.manager = manager;
     }
 
     public Date getStartDate() {
@@ -98,14 +54,6 @@ public class MachineModel extends AbstractModel {
 
     public Date getRenewalDate() {
         return renewalDate;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
     }
 
     public void setRenewalDate(Date renewalDate) {
@@ -142,22 +90,6 @@ public class MachineModel extends AbstractModel {
 
     public void setKeepAlive(Date keepAlive) {
         this.keepAlive = keepAlive;
-    }
-
-    public int getReadTimeout() {
-        return readTimeout;
-    }
-
-    public void setReadTimeout(int readTimeout) {
-        this.readTimeout = readTimeout;
-    }
-
-    public int getSendPeriod() {
-        return sendPeriod;
-    }
-
-    public void setSendPeriod(int sendPeriod) {
-        this.sendPeriod = sendPeriod;
     }
 
 }

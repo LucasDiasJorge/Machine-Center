47b6b92c927211b0b4ca11d37e33522811989e3e
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-04-24 17:03:53 -0300

    Update ConfigRepository.java

--- src/main/java/com/project/reader/repository/ConfigRepository.java
+++ src/main/java/com/project/reader/repository/ConfigRepository.java
@@ -1,11 +1,17 @@
 package com.project.reader.repository;
 
 import com.project.reader.model.ConfigModel;
+import com.project.reader.model.MachineModel;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 @Primary
 public interface ConfigRepository extends JpaRepository<ConfigModel, Long>{
 
+
+    @Query("SELECT c FROM ConfigModel c WHERE c.deletedBy IS NULL AND c.machineModel.id = :machineId")
+    ConfigModel findByMachine(@Param("machineId") Long machineId);
 
 }

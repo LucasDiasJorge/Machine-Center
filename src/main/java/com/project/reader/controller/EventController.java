60440de59b8781983cdd6020989bc0cf6785d17d
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-04-24 17:03:42 -0300

    Update EventController.java

--- src/main/java/com/project/reader/controller/EventController.java
+++ src/main/java/com/project/reader/controller/EventController.java
@@ -1,29 +1,113 @@
 package com.project.reader.controller;
 
+import com.project.reader.exception.AppException;
+import com.project.reader.model.MachineModel;
+import com.project.reader.services.ConfigService;
 import com.project.reader.services.EventService;
-import org.springframework.data.repository.query.Param;
+import com.project.reader.services.MachineService;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/event")
 public class EventController {
 
     private final EventService service;
+    private final MachineService machineService;
+    private final ConfigService configService;
 
-    public EventController(EventService service) {
+    public EventController(EventService service, MachineService machineService, ConfigService configService) {
         this.service = service;
+        this.machineService = machineService;
+        this.configService = configService;
     }
 
-    @PreAuthorize("hasRole('PORTAL')")
-    @GetMapping("/something/{serial}")
-    @Transactional
-    public List<String> something(@PathVariable String serial, @Param("lastModifiedDate") Long lastModifiedDate) {
+    @PreAuthorize("hasRole('MACHINE')")
+    @GetMapping("/keepalive/{serial}")
+    public Map<String,Object> keepalive(@PathVariable String serial) {
 
-        return null;
+        Map<String,Object> ret = new HashMap<>();
+
+        MachineModel machine = machineService.findBySerial(serial);
+
+        ret.put("In",machine.isIn() ? 1 : 0);
+        ret.put("Out",machine.isOut() ? 1 : 0);
+        ret.put("Config",machine.isGetConfig() ? 1 : 0);
+        ret.put("Reboot",machine.isReboot() ? 1 : 0);
+        ret.put("Destination",configService.getConfig(machine).getDestination());
+
+        return ret;
+
+    }
+
+    @PreAuthorize("hasRole('MACHINE')")
+    @PutMapping("/in/{serial}")
+    public Map<String,Integer> in(@PathVariable String serial, Principal principal) throws AppException {
+
+        Map<String,Integer> ret = new HashMap<>();
+
+        MachineModel machine = machineService.findBySerial(serial);
+
+        machine.setIn(true);
+
+        machineService.update(machine.getId(),machine, principal);
+
+        return ret;
+
+    }
+
+    @PreAuthorize("hasRole('MACHINE')")
+    @GetMapping("/out/{serial}")
+    public Map<String,Integer> out(@PathVariable String serial) {
+
+        Map<String,Integer> ret = new HashMap<>();
+
+        MachineModel machine = machineService.findBySerial(serial);
+
+        ret.put("In",machine.isIn() ? 1 : 0);
+        ret.put("Out",machine.isOut() ? 1 : 0);
+        ret.put("Config",machine.isGetConfig() ? 1 : 0);
+        ret.put("Reboot",machine.isReboot() ? 1 : 0);
+
+        return ret;
+
+    }
+
+    @PreAuthorize("hasRole('MACHINE')")
+    @GetMapping("/reboot/{serial}")
+    public Map<String,Integer> reboot(@PathVariable String serial) {
+
+        Map<String,Integer> ret = new HashMap<>();
+
+        MachineModel machine = machineService.findBySerial(serial);
+
+        ret.put("In",machine.isIn() ? 1 : 0);
+        ret.put("Out",machine.isOut() ? 1 : 0);
+        ret.put("Config",machine.isGetConfig() ? 1 : 0);
+        ret.put("Reboot",machine.isReboot() ? 1 : 0);
+
+        return ret;
+
+    }
+
+    @PreAuthorize("hasRole('MACHINE')")
+    @GetMapping("/config/{serial}")
+    public Map<String,Integer> config(@PathVariable String serial) {
+
+        Map<String,Integer> ret = new HashMap<>();
+
+        MachineModel machine = machineService.findBySerial(serial);
+
+        ret.put("In",machine.isIn() ? 1 : 0);
+        ret.put("Out",machine.isOut() ? 1 : 0);
+        ret.put("Config",machine.isGetConfig() ? 1 : 0);
+        ret.put("Reboot",machine.isReboot() ? 1 : 0);
+
+        return ret;
 
     }
 

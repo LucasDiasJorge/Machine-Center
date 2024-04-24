6e8ca21b4e5d43136411cc0eb03959f008bd4de0
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-04-24 17:04:11 -0300

    Update MachineService.java

--- src/main/java/com/project/reader/services/MachineService.java
+++ src/main/java/com/project/reader/services/MachineService.java
@@ -1,14 +1,18 @@
 package com.project.reader.services;
 
+import com.project.reader.exception.AppException;
 import com.project.reader.model.MachineModel;
 import com.project.reader.repository.MachineRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.security.Principal;
+import java.util.Date;
+import java.util.Optional;
+
 
 @Service
-@Transactional
 public class MachineService extends AbstractService<MachineModel, MachineRepository> {
 
 
@@ -16,5 +20,36 @@ public class MachineService extends AbstractService<MachineModel, MachineReposit
     public MachineService(MachineRepository repository) {
         super(repository);
     }
+
+    public MachineModel findBySerial(String serial){
+        return repository.findBySerial(serial);
+    }
+
+    public MachineModel update(Long id, MachineModel machineModel, Principal principal) throws AppException {
+
+        Date now = new Date();
+
+        MachineModel machineVerify = findBySerial(machineModel.getSerial());
+
+        machineVerify.setLastModifiedDate(now);
+
+        return super.update(id, machineModel, machineVerify, principal);
+    }
+
+    @Override
+    public MachineModel save(MachineModel machineModel) throws AppException {
+
+        Optional<MachineModel> machineModelOpt = Optional.ofNullable(repository.findBySerial(machineModel.getSerial()));
+
+        if (machineModelOpt.isPresent()) {
+            throw new AppException("Machine with serial '" + machineModelOpt.get().getSerial() + "' already exists!", 422, null);
+        }
+
+        machineModel.setCreatedBy(1L);
+        machineModel.setUpdatedBy(1L);
+
+        return super.save(machineModel);
+    }
+
 
 }

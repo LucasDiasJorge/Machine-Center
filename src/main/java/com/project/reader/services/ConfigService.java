a8c2071a615cb1709c0dff87c7ab993c43e4a2c9
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-04-24 17:04:03 -0300

    Update ConfigService.java

--- src/main/java/com/project/reader/services/ConfigService.java
+++ src/main/java/com/project/reader/services/ConfigService.java
@@ -2,6 +2,7 @@ package com.project.reader.services;
 
 import com.project.reader.model.ConfigModel;
 import com.project.reader.model.MachineModel;
+import com.project.reader.repository.MachineRepository;
 import org.springframework.stereotype.Service;
 
 import com.project.reader.repository.ConfigRepository;
@@ -10,14 +11,20 @@ import com.project.reader.repository.ConfigRepository;
 public class ConfigService {
 
     public final ConfigRepository repository;
+    public final MachineRepository machineRepository;
 
-    public ConfigService(ConfigRepository repository) {
+    public ConfigService(ConfigRepository repository, MachineRepository machineRepository) {
         this.repository = repository;
+        this.machineRepository = machineRepository;
+    }
+
+    public ConfigModel getConfig(String serial) {
+        return repository.findByMachine(machineRepository.findBySerial(serial).getId());
     }
 
 
-    public String getConfig(String serial) {
-        return "config";
+    public ConfigModel getConfig(MachineModel machineModel) {
+        return repository.findByMachine(machineModel.getId());
     }
 
     public String saveConfig(String config, MachineModel portalModel){

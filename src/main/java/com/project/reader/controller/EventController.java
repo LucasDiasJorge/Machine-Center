cfba81623d7df72f18dc6e391888a677e02d5fe5
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-05-14 13:59:18 -0300

    Adding filter (int field)

--- src/main/java/com/project/reader/controller/EventController.java
+++ src/main/java/com/project/reader/controller/EventController.java
@@ -2,6 +2,7 @@ package com.project.reader.controller;
 
 import com.project.reader.exception.AppException;
 import com.project.reader.model.MachineModel;
+import com.project.reader.repository.MachineRepository;
 import com.project.reader.services.ConfigService;
 import com.project.reader.services.EventService;
 import com.project.reader.services.MachineService;
@@ -9,7 +10,9 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -28,16 +31,24 @@ public class EventController {
 
     @PreAuthorize("hasRole('MACHINE')")
     @GetMapping("/keepalive/{serial}")
-    public Map<String,Object> keepalive(@PathVariable String serial) {
+    public Map<String,Object> keepalive(@PathVariable String serial, Principal principal) throws AppException {
 
         Map<String,Object> ret = new HashMap<>();
 
+        Date now = new Date();
+
         MachineModel machine = machineService.findBySerial(serial);
+        machine.setKeepAlive(now);
+        machine.setIn(false);
+        machine.setOut(false);
+        machineService.update(machine.getId(),machine,principal);
 
         ret.put("In",machine.isIn() ? 1 : 0);
         ret.put("Out",machine.isOut() ? 1 : 0);
         ret.put("Config",machine.isGetConfig() ? 1 : 0);
         ret.put("Reboot",machine.isReboot() ? 1 : 0);
+        ret.put("Read",machine.isRead() ? 1 : 0);
+        ret.put("Filter",machine.getFilterTimeout());
         ret.put("Destination",configService.getConfig(machine).getDestination());
 
         return ret;
@@ -45,33 +56,33 @@ public class EventController {
     }
 
     @PreAuthorize("hasRole('MACHINE')")
-    @PutMapping("/in/{serial}")
-    public Map<String,Integer> in(@PathVariable String serial, Principal principal) throws AppException {
+    @PutMapping("/in/{companyId}")
+    public Map<String,Integer> in(@PathVariable Long companyId, Principal principal) throws AppException {
 
         Map<String,Integer> ret = new HashMap<>();
 
-        MachineModel machine = machineService.findBySerial(serial);
+        List<MachineModel> machines = machineService.findByCompanyId(companyId);
 
-        machine.setIn(true);
-
-        machineService.update(machine.getId(),machine, principal);
+        for (MachineModel machine:machines) {
+            machine.setIn(true);
+            machineService.update(machine.getId(),machine, principal);
+        }
 
         return ret;
 
     }
 
     @PreAuthorize("hasRole('MACHINE')")
-    @GetMapping("/out/{serial}")
-    public Map<String,Integer> out(@PathVariable String serial) {
-
+    @GetMapping("/out/{companyId}")
+    public Map<String,Integer> out(@PathVariable Long companyId, Principal principal) throws AppException {
         Map<String,Integer> ret = new HashMap<>();
 
-        MachineModel machine = machineService.findBySerial(serial);
+        List<MachineModel> machines = machineService.findByCompanyId(companyId);
 
-        ret.put("In",machine.isIn() ? 1 : 0);
-        ret.put("Out",machine.isOut() ? 1 : 0);
-        ret.put("Config",machine.isGetConfig() ? 1 : 0);
-        ret.put("Reboot",machine.isReboot() ? 1 : 0);
+        for (MachineModel machine:machines) {
+            machine.setIn(true);
+            machineService.update(machine.getId(),machine, principal);
+        }
 
         return ret;
 
@@ -85,10 +96,8 @@ public class EventController {
 
         MachineModel machine = machineService.findBySerial(serial);
 
-        ret.put("In",machine.isIn() ? 1 : 0);
-        ret.put("Out",machine.isOut() ? 1 : 0);
-        ret.put("Config",machine.isGetConfig() ? 1 : 0);
-        ret.put("Reboot",machine.isReboot() ? 1 : 0);
+        // Switcher
+        machine.setReboot(!machine.isReboot());
 
         return ret;
 
@@ -102,10 +111,8 @@ public class EventController {
 
         MachineModel machine = machineService.findBySerial(serial);
 
-        ret.put("In",machine.isIn() ? 1 : 0);
-        ret.put("Out",machine.isOut() ? 1 : 0);
-        ret.put("Config",machine.isGetConfig() ? 1 : 0);
-        ret.put("Reboot",machine.isReboot() ? 1 : 0);
+        // Switcher
+        machine.setConfig(!machine.isConfig());
 
         return ret;
 
